57efe425dc842866bd31c43aa8a60e346c6f7625
Tomeu Cabot <tomeu.cabot@trivago.com>
2018-05-25 16:17:48 +0200

    try rollup

---
+++ lib/components/button/button.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="react" />
+import * as React from "react";
+import "./button.css";
+export interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+}
+export declare const Button: React.StatelessComponent<Props>;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,1 @@
-import React from 'react';
-import './index.css';
-
-interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> { }
-
-export const Button: React.StatelessComponent<Props> = (props) => {
-  return <button className="btn" {...props}>{props.children}</button>;
-}
+export { Button } from './components/button';
